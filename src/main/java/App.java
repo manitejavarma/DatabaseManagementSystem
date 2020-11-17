dcf633bbbbc9c7618e618ba37fbd3d9d45e935cf
Mani Teja Varma <mn947410@dal.ca>
2020-11-16 23:41:32 -0400

    Manage CSV

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,7 +1,20 @@
+import org.apache.commons.csv.CSVRecord;
 
+import java.io.IOException;
 
 public class App {
     public static void main(String[] args){
         System.out.println("Hello World!");
+        try {
+            CSVManager.setHeaders();
+          CSVManager.insertCSV();
+            CSVManager.insertCSV();
+            //CSVManager.readCSV();
+//            CSVManager.getHeaders();
+            //CSVManager.setHeaders();
+            CSVManager.updateCSV();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
