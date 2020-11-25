abd4a4ccd78ceb9efe05250b954a2971c25cc386
Rajveen Singh Chandhok <rj660238@dal.ca>
2020-11-25 19:22:23 -0400

    Added support for primary and foreign keys in the metadata

--- src/main/java/metadata/Metadata.java
+++ src/main/java/metadata/Metadata.java
@@ -3,11 +3,14 @@ package metadata;
 import attributetype.AttributeType;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class Metadata {
     private String tableName;
     private Map<String, AttributeType> columns;
+    private List<String> primaryKeys;
+    private List<ReferenceDefinition> foreignKeys;
 
     public Metadata() {
         columns = new HashMap<>();
@@ -27,5 +30,21 @@ public class Metadata {
 
     public void setColumns(Map<String, AttributeType> columns) {
         this.columns = columns;
+    }
+
+    public List<String> getPrimaryKeys() {
+        return primaryKeys;
+    }
+
+    public void setPrimaryKeys(List<String> primaryKeys) {
+        this.primaryKeys = primaryKeys;
+    }
+
+    public List<ReferenceDefinition> getForeignKeys() {
+        return foreignKeys;
+    }
+
+    public void setForeignKeys(List<ReferenceDefinition> foreignKeys) {
+        this.foreignKeys = foreignKeys;
     }
 }
