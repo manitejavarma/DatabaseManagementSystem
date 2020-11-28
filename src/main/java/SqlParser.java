f172f01a4f49dda2aa325cd85dbb9ae985832ced
Shwetha Subash <shwetha.subash@dal.ca>
2020-11-28 13:42:55 -0400

    changes for select query

--- src/main/java/SqlParser.java
+++ src/main/java/SqlParser.java
@@ -1,4 +1,6 @@
 //https://regexlib.com/REDetails.aspx?regexp_id=945&AspxAutoDetectCookieSupport=1
+import java.util.HashMap;
+import java.util.Map;
 import java.util.logging.Logger;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -8,17 +10,32 @@ import utils.Constants;
 public class SqlParser implements ISqlParser {
 	private final static Logger logger = Logger.getLogger(SqlParser.class.getName());
     
-	public int validateQuery(String query) {
+	public void validateQuery(String query) {
 		logger.info("Invoked validateQuery method");
-		Pattern pattern = Pattern.compile(Constants.REGEX, Pattern.CASE_INSENSITIVE);
+		if(query.toLowerCase().contains("select"))
+		{
+			selectQuery(query);
+		}
+
+		
+
+	}
+	
+	void selectQuery(String query) {
+		Map<String,String> selectFields = new HashMap<String, String>();
+		Pattern pattern = Pattern.compile(Constants.SELECT_REGEX, Pattern.CASE_INSENSITIVE);
 		Matcher matcher = pattern.matcher(query);
 		boolean matchFound = matcher.find();
 		if (matchFound) {
-			return Constants.ONE;
-		} else {
-			return Constants.ZERO;
+			selectFields.put("selectColumns", matcher.group(1));
+			selectFields.put("tableName", matcher.group(2));
+			selectFields.put("whereColumn", matcher.group(3));
+			selectFields.put("whereValue", matcher.group(4));
 		}
-
+		
+		//call to semantic parser
+		//if valid, func call to execute the query, pass the map as input 
+		
 	}
 
 }
--- src/main/java/utils/Constants.java
+++ src/main/java/utils/Constants.java
@@ -3,8 +3,8 @@ package utils;
 public class Constants {
 	
 	//Regular expression for all basic SQL queries
-	public static String REGEX ="(SELECT\\s[\\w\\*\\)\\(\\,\\s]+\\sFROM\\s[\\w]+)|(UPDATE\\s[\\w]+\\sSET\\s[\\w\\,\\'\\=]+)|(INSERT\\sINTO\\s[\\d\\w]+[\\s\\w\\d\\)\\(\\,]*\\sVALUES\\s\\([\\d\\w\\'\\,\\)]+)|(DELETE\\sFROM\\s[\\d\\w\\'\\=]+)";
-	public static String QUERY="SELECT * from table";
-	public static int ONE = 1;
-	public static int ZERO = 0;
+	public static String REGEX =  "(create\\s+database\\s+\\w+)|(create\\s+table\\s+\\w+\\s*\\((?:[^()]+|\\([^()]+\\))*\\))|(SELECT\\s[\\w\\*\\)\\(\\,\\s]+\\sFROM\\s[\\w]+)|(UPDATE\\s[\\w]+\\sSET\\s[\\w\\,\\'\\=]+)|(INSERT\\sINTO\\s[\\d\\w]+[\\s\\w\\d\\)\\(\\,]*\\sVALUES\\s\\([\\d\\w\\'\\,\\)]+)|(DELETE\\sFROM\\s[\\d\\w\\'\\=]+)";
+	public static String QUERY="SELECT * FROM table where col=\"val\"";
+	public static String SELECT_REGEX = "^SELECT\\s([\\w\\*\\)\\(\\,\\s]+)\\sFROM\\s([\\w]+)\\sWHERE\\s(\\w+)\\s*\\=\\s*([\"\\w]+);$";
+	
 }
